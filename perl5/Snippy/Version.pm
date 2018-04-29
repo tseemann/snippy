64a90a2302dfb116bedcc2b6031dcd3e5001807f
Torsten Seemann <torsten.seemann@gmail.com>
2018-04-29 10:38:36 +1000

    Issue #145 | Harmonize version for both tools

---
+++ perl5/Snippy/Version.pm
@@ -0,0 +1,26 @@
+package Snippy::Version;
+
+use base Exporter;
+@EXPORT_OK = qw(version);
+%EXPORT_TAGS = ( 'all' =>  [ @EXPORT_OK ] );
+
+our $VERSION = "4.0-dev2";
+
+use strict;
+use File::Basename;
+
+sub version {
+  return $VERSION;
+}
+
+sub command {
+  return basename($0);
+}
+
+sub version_string {
+  my $self = shift;
+  return $self->command.' '.$self->version;
+}
+
+1;
+
