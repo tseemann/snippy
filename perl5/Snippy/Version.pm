509162e1241557d64061196d229ef680eebbc36b
Torsten Seemann <torsten.seemann@gmail.com>
2018-07-07 17:23:50 +1000

    Use date version for pre release

--- perl5/Snippy/Version.pm
+++ perl5/Snippy/Version.pm
@@ -4,7 +4,7 @@ use base Exporter;
 @EXPORT_OK = qw(version);
 %EXPORT_TAGS = ( 'all' =>  [ @EXPORT_OK ] );
 
-our $VERSION = "4.0-dev2";
+our $VERSION = "4.0-pre_20180707";
 
 use strict;
 use File::Basename;
